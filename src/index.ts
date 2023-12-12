b6f2ef6d92af315f3de55753d215d2210b5a75fd
yangkun <yangkun@icourt.cc>
2023-12-12 13:39:53 +0800

    perf: 优化冗余代码

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
-import EleEasyTable from './components/EleEasyTable.vue';
-import CustomColumn from './components/CustomColumn/index.vue';
-import EleEasyTableEdit from './components/EleEasyTableEdit.vue';
+// import EleEasyTable from './components/EleEasyTable.vue';
+// import CustomColumn from './components/CustomColumn/index.vue';
+// import EleEasyTableEdit from './components/EleEasyTableEdit.vue';
 import ProTable from './components/ProTable/index.vue';
 import { App } from 'vue';
 
 export default {
   install: (app: App) => {
-    app.component('EleEasyTable', EleEasyTable);
-    app.component('CustomColumn', CustomColumn);
-    app.component('EleEasyTableEdit', EleEasyTableEdit);
-    app.component('ProTable', ProTable);
+    // app.component('EleEasyTable', EleEasyTable);
+    // app.component('CustomColumn', CustomColumn);
+    // app.component('EleEasyTableEdit', EleEasyTableEdit);
+    // app.component('ProTable', ProTable);
     app.component('TablePlus', ProTable);
   },
 };
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
+// webpack.config.js
+const AutoImport = require('unplugin-auto-import/webpack')
+const Components = require('unplugin-vue-components/webpack')
+const { ElementPlusResolver } = require('unplugin-vue-components/resolvers')
 module.exports = defineConfig({
   transpileDependencies: true,
   css: {
@@ -6,9 +10,17 @@ module.exports = defineConfig({
   },
   outputDir: "docs",
   productionSourceMap: false,
-  configureWebpack: {
-    externals: process.env.NODE_ENV === 'production' ? {
-      "vuedraggable": "vuedraggable",
-    } : {}
+  configureWebpack: (config) => {
+    config.plugins = [
+      ...config.plugins,
+      AutoImport({
+        resolvers: [ElementPlusResolver()],
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()],
+      }),
+    ]
+
+
   },
 })
